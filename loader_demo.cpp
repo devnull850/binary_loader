c05b7669acfd97d08eed9012ce53a95a99d1bd0a
devnull850 <dev.null850@gmail.com>
2021-09-23 05:32:25 -0500

    Integrate Capstone API

--- loader_demo.cpp
+++ loader_demo.cpp
@@ -2,13 +2,22 @@
 #include <cstdint>
 #include <string>
 
+#include <capstone/capstone.h>
+
 #include "loader.h"
 
 int main(int argc, char *argv[]) {
     Binary bin;
+#ifdef DEBUG
     Section *sec;
     Symbol *sym;
+#endif
+    Section *text;
     std::string fname;
+    csh handle;
+    cs_insn *instructions;
+    cs_err e;
+    size_t cnt;
 
     if (argc < 2) {
         printf("Usage: %s <binary>\n", argv[0]);
@@ -21,6 +30,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+#ifdef DEBUG
     printf("load binary '%s' %s/%s (%u bits) entry@0x%016jx\n",
             bin.filename.c_str(), bin.type_str.c_str(),
             bin.arch_str.c_str(), bin.bits, bin.entry);
@@ -39,6 +49,33 @@ int main(int argc, char *argv[]) {
                     (sym->type & Symbol::SYM_TYPE_FUNC) ? "FUNC" : "");
         }
     }
+#endif
+
+    if ((text = bin.get_text_section()) == NULL) {
+        puts("Nothing to disassemble");
+        return 0;
+    }
+
+    if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
+        e = cs_errno(handle);
+        puts(cs_strerror(e));
+        exit(e);
+    }
+
+    if (!(cnt = cs_disasm(handle, text->bytes, text->size, text->vma, 0, &instructions))) {
+        e = cs_errno(handle);
+        puts(cs_strerror(e));
+        exit(e);
+    }
+
+    for (size_t i = 0; i < cnt; ++i) {
+        printf("0x%lx:\t%s\t\t%s\n", instructions[i].address, instructions[i].mnemonic,
+					instructions[i].op_str);
+    }
+
+    cs_free(instructions, cnt);
+
+    cs_close(&handle);
 
     unload_binary(&bin);
 
