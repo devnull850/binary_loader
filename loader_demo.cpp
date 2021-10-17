05ba26267794bf936a50dd96007471b948efa13a
devnull850 <dev.null850@gmail.com>
2021-10-17 12:45:57 -0400

    Implement recursive disassembly algorithm

--- loader_demo.cpp
+++ loader_demo.cpp
@@ -1,23 +1,24 @@
 #include <cstdio>
-#include <cstdint>
+#include <map>
+#include <queue>
 #include <string>
 
 #include <capstone/capstone.h>
 
 #include "loader.h"
 
+using namespace std;
+
+int disasm(Binary *bin);
+void print_ins(cs_insn *ins);
+bool is_cs_cflow_group(uint8_t g);
+bool is_cs_cflow_ins(cs_insn *ins);
+bool is_cs_unconditional_cflow_ins(cs_insn *ins);
+uint64_t get_cs_ins_immediate_target(cs_insn *ins);
+
 int main(int argc, char *argv[]) {
     Binary bin;
-#ifdef DEBUG
-    Section *sec;
-    Symbol *sym;
-#endif
-    Section *text;
-    std::string fname;
-    csh handle;
-    cs_insn *instructions;
-    cs_err e;
-    size_t cnt;
+    string fname;
 
     if (argc < 2) {
         printf("Usage: %s <binary>\n", argv[0]);
@@ -25,59 +26,162 @@ int main(int argc, char *argv[]) {
     }
 
     fname.assign(argv[1]);
-
     if (load_binary(fname, &bin, Binary::BIN_TYPE_AUTO) < 0) {
         return 1;
     }
 
-#ifdef DEBUG
-    printf("load binary '%s' %s/%s (%u bits) entry@0x%016jx\n",
-            bin.filename.c_str(), bin.type_str.c_str(),
-            bin.arch_str.c_str(), bin.bits, bin.entry);
-
-    for (size_t i = 0; i < bin.sections.size(); ++i) {
-        sec = &bin.sections[i];
-        printf("  0x%016jx %-8ju %-20s %s\n", sec->vma, sec->size, sec->name.c_str(),
-                sec->type == Section::SEC_TYPE_CODE ? "CODE" : "DATA");
+    if (disasm(&bin) < 0) {
+        return 1;
     }
 
-    if (bin.symbols.size() > 0) {
-        puts("scanned symbol tables");
-        for (size_t i = 0; i < bin.symbols.size(); ++i) {
-            sym = &bin.symbols[i];
-            printf("  %-40s 0x%016jx %s\n", sym->name.c_str(), sym->addr,
-                    (sym->type & Symbol::SYM_TYPE_FUNC) ? "FUNC" : "");
-        }
-    }
-#endif
+    unload_binary(&bin);
 
-    if ((text = bin.get_text_section()) == NULL) {
-        puts("Nothing to disassemble");
+    return 0;
+}
+
+int disasm(Binary *bin) {
+    csh dis;
+    cs_insn *cs_ins;
+    Section *text;
+    size_t n;
+    const uint8_t *pc;
+    uint64_t addr, offset, target;
+    queue<uint64_t> q;
+    map<uint64_t, bool> seen;
+
+    if (!(text = bin->get_text_section())) {
+        fprintf(stderr, "Nothing to disassemble\n");
         return 0;
     }
 
-    if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
-        e = cs_errno(handle);
-        puts(cs_strerror(e));
-        exit(e);
+    if (cs_open(CS_ARCH_X86, CS_MODE_64, &dis) != CS_ERR_OK) {
+        fprintf(stderr, "Failed to open Capstone\n");
+        return -1;
     }
 
-    if (!(cnt = cs_disasm(handle, text->bytes, text->size, text->vma, 0, &instructions))) {
-        e = cs_errno(handle);
-        puts(cs_strerror(e));
-        exit(e);
+    cs_option(dis, CS_OPT_DETAIL, CS_OPT_ON);
+
+    if (!(cs_ins = cs_malloc(dis))) {
+        fprintf(stderr, "Out of memory\n");
+        cs_close(&dis);
+        return -1;
     }
 
-    for (size_t i = 0; i < cnt; ++i) {
-        printf("0x%lx:\t%s\t\t%s\n", instructions[i].address, instructions[i].mnemonic,
-					instructions[i].op_str);
+    addr = bin->entry;
+    if (text->contains(addr)) {
+        q.push(addr);
     }
 
-    cs_free(instructions, cnt);
+    printf("entry point: 0x%016jx\n", addr);
 
-    cs_close(&handle);
+    for (auto &sym: bin->symbols) {
+        if (sym.type == Symbol::SYM_TYPE_FUNC && text->contains(sym.addr)) {
+            q.push(sym.addr);
+            printf("function symbol: 0x%016jx\n", sym.addr);
+        }
+    }
 
-    unload_binary(&bin);
+    while (!q.empty()) {
+        addr = q.front();
+        q.pop();
+
+        if (seen[addr]) {
+            continue;
+        }
+
+        offset = addr - text->vma;
+        pc     = text->bytes + offset;
+        n      = text->size - offset;
+
+        while (cs_disasm_iter(dis, &pc, &n, &addr, cs_ins)) {
+            if (cs_ins->id == X86_INS_INVALID || cs_ins->size == 0) {
+                break;
+            }
+
+            seen[cs_ins->address] = true;
+            print_ins(cs_ins);
+
+            if (is_cs_cflow_ins(cs_ins)) {
+                target = get_cs_ins_immediate_target(cs_ins);
+
+                if (target && !seen[target] && text->contains(target)) {
+                    q.push(target);
+                    printf("   -> new target: 0x%016jx\n", target);
+                }
+
+                if (is_cs_unconditional_cflow_ins(cs_ins)) {
+                    break;
+                }
+            }
+            else if (cs_ins->id == X86_INS_HLT) {
+                break;
+            }
+        }
+
+        puts("----------");
+    }
+
+    cs_free(cs_ins, 1);
+    cs_close(&dis);
+
+    return 0;
+}
+
+void print_ins(cs_insn *ins) {
+    printf("0x%016jx: ", ins->address);
+    for (size_t i = 0; i < 16; ++i) {
+        if (i < ins->size) {
+            printf("%02x ", ins->bytes[i]);
+        }
+        else {
+            printf("   ");
+        }
+    }
+
+    printf("%-12s %s\n", ins->mnemonic, ins->op_str);
+}
+
+bool is_cs_cflow_group(uint8_t g) {
+    return g == CS_GRP_JUMP || g == CS_GRP_CALL ||
+            g == CS_GRP_RET || g == CS_GRP_IRET;
+}
+
+bool is_cs_cflow_ins(cs_insn *ins) {
+    for (size_t i = 0; i < ins->detail->groups_count; ++i) {
+        if (is_cs_cflow_group(ins->detail->groups[i])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool is_cs_unconditional_cflow_ins(cs_insn *ins) {
+    switch (ins->id) {
+        case X86_INS_JMP:
+        case X86_INS_LJMP:
+        case X86_INS_RET:
+        case X86_INS_RETF:
+        case X86_INS_RETFQ:
+            return true;
+        default:
+            return false;
+    }
+}
+
+uint64_t get_cs_ins_immediate_target(cs_insn *ins) {
+    cs_x86_op *cs_op;
+
+    for (size_t i = 0; i < ins->detail->groups_count; ++i) {
+        if (is_cs_cflow_group(ins->detail->groups[i])) {
+            for (size_t j = 0; j < ins->detail->x86.op_count; ++j) {
+                cs_op = &ins->detail->x86.operands[j];
+                if (cs_op->type == X86_OP_IMM) {
+                    return cs_op->imm;
+                }
+            }
+        }
+    }
 
     return 0;
 }
